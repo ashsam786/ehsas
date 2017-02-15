308a40007e4fd82f288579c6a3d50566b4a55312
Amir Hanga <amirh@chetu.com>
2017-02-15 16:54:45 +0530

    form submit functionality complete

--- js/script.js
+++ js/script.js
@@ -1,14 +1,5 @@
-
-
-
-
-
-
-
-  
-  jQuery().ready(function() {
-    
-    jQuery.validator.addMethod('phoneUS', function(phone_number, element) {
+jQuery().ready(function() {
+	jQuery.validator.addMethod('phoneUS', function(phone_number, element) {
         phone_number = phone_number.replace(/\s+/g, ''); 
         return this.optional(element) || phone_number.length > 9 &&
             phone_number.match(/^(\+91-?)?(\([2-9]\d{2}\)|[2-9]\d{2})-?[2-9]\d{2}-?\d{4}$/);
@@ -93,5 +84,4 @@
       $(".frm").hide("fast");
       $("#sf2").show("slow");
     });
-
-  });
+});
--- process_form.php
+++ process_form.php
@@ -1,80 +1,41 @@
-
-
-
-
 <?php 
 
 $dsn = 'mysql:dbname=ehsas;host=127.0.0.1';
 $user = 'root';
 $password = '';
+$data = ['result' => false];
 
 try {
     $dbh = new PDO($dsn, $user, $password);
 	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    echo 'Connection failed: ' . $e->getMessage();
+    $data['msg'] = 'Connection failed: ' . $e->getMessage();
 }
 
-//echo '<pre>'; print_r($_POST); die;
+$column_names = "`last_time_donated`,`name`,`address`,`contact`,`alternate_contact`,`blood_group`,`district`,`nearby_hospital`,`how_you_know_us`";
 
+$value_placeholders = ":last_time_donated, :name, :address, :contact, :alternate_contact, :blood_group, :district, :nearby_hospital, :how_you_know_us";
 
- => 2017-02-14
-     => fsgdfgdfg
-    [uaddress] => aaaa@ert.lo
-    [uphone] => 9999999999
-    [alternate_uphone] => 6666666666
-    [ubloodGroup] => B Positive
-    [udistrict] => Bandipora
-    [unearbyhospital] => S.M.S.H Hospital
-    [uhowuknow] => fgh fdgh dfgh dfgh
+$values = [
+	':last_time_donated'=> trim($_POST['lastTimeDonated']),
+	':name'				=> trim($_POST['uname']),
+	':address'			=> trim($_POST['uaddress']),
+	':contact'			=> trim($_POST['uphone']),
+	':alternate_contact'=> trim($_POST['alternate_uphone']),
+	':blood_group'		=> trim($_POST['ubloodGroup']),
+	':district'			=> trim($_POST['udistrict']),
+	':nearby_hospital'	=> trim($_POST['unearbyhospital']),
+	':how_you_know_us'	=> trim($_POST['uhowuknow'])
+];
 
+try{
+	$sql = "INSERT INTO donor_list ({$column_names}) VALUES ({$value_placeholders})";
+	$sth = $dbh->prepare($sql);
+	$res = $sth->execute($values);
+	$data['result'] = true;
+} catch (PDOException $e) {
+    echo 'Error : ' . $e->getMessage();
+}
 
-
-
-
-
-$last_time_donated	= $_POST['lastTimeDonated'];
-$name				= $_POST['uname'];
-$address			= $_POST
-$contact			= $_POST
-$alternate_contact	= $_POST
-$blood_group		= $_POST
-$district			= $_POST
-$nearby_hospital	= $_POST
-$how_you_know_us	= $_POST
-$timestamp			= $_POST
-
-
-
-$last_time_donated
-$name
-$address
-$contact
-$alternate_contact
-$blood_group
-$district
-$nearby_hospital
-$how_you_know_us
-$timestamp
-
-
-$column_names = ['last_time_donated','name','address','contact','alternate_contact','blood_group','district','nearby_hospital','how_you_know_us','timestamp'];
-
-$sql = "INSERT INTO donor_list {$column_names} OUTPUT INSERTED.id VALUES (?)";
-$sth = $dbh->prepare($sql);
-$sth->bind_param(); 
-$sth->execute(array('widgets'));
-$temp = $sth->fetch(PDO::FETCH_ASSOC);
-
-
-
-
-/* $stmt = $dbh->prepare("SELECT * FROM donor_list"); 
-$stmt->execute();
-$result = $stmt->fetchAll(PDO::FETCH_ASSOC); 
-echo '<pre>'; print_r($result); die;  */
-
-
-
-
- ?>
+echo json_encode($data);
+die;
