e756aa091bec5a8ea74dac2ae5e36763743c1474
Amir Samad Hanga <amirprimetime@gmail.com>
2017-03-02 12:14:11 +0530

    resolved issue with query join in getDonorBycontact function

--- application/models/Donor_model.php
+++ application/models/Donor_model.php
@@ -12,8 +12,8 @@ class donor_model extends CI_Model{
 		try{
 			$this->db->select('donor_list.*, countries.country as country_name, states.state as state_name, cities.city as city_name');
 			$this->db->join('countries', 'donor_list.country = countries.id', 'left');
-			$this->db->join('states', 'donor_list.state = states.id' 'left');
-			$this->db->join('cities', 'donor_list.city = cities.id' 'left');
+			$this->db->join('states', 'donor_list.state = states.id', 'left');
+			$this->db->join('cities', 'donor_list.city = cities.id', 'left');
 			$qry = $this->db->get_where($this->table, ['contact' => $contact]);
 
 			if($qry->num_rows() != 1){
