9030b87efb736b0ba4bc9e8cac42ad9129b37f05
Amir Hanga <amirh@chetu.com>
2017-03-16 23:17:32 +0530

    remove novalildate attribute from form tag submit blood request

--- application/views/blood/requirement.php
+++ application/views/blood/requirement.php
@@ -41,7 +41,7 @@
 	<?php } ?>
 		<div class="container">
 			<div class="tim-row" id="currentRequimentsTable">
-				<form class="form" method="post" action="<?php echo base_url('blood/process_requirement'); ?>" novalidate>
+				<form class="form" method="post" action="<?php echo base_url('blood/process_requirement'); ?>">
 					<div class="content">
 						<div class="row">
 							<div class="header header-danger">
@@ -126,7 +126,7 @@
 							<div class="col-md-4">
 								<div class="form-group label-floating">
 									<label class="control-label" for="requiredOn">Required on/before</label>
-									<input type="text" class="datepicker form-control required" name="requiredOn" id="requiredOn" value="mm/dd/yyyy" required>
+									<input type="date" class="datepicker form-control required" name="requiredOn" id="requiredOn" value="mm/dd/yyyy" required>
 									<span class="material-input"></span>
 								</div>
 							</div>	
