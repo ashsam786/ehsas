8f11542ba299a6e3bddac78daa736598be3f438e
Amir Samad Hanga <amirprimetime@gmail.com>
2017-03-22 11:11:56 +0530

    update about us page

--- application/views/aboutus.php
+++ application/views/aboutus.php
@@ -17,12 +17,10 @@
 	<div class="section section-basic">
 		<div class="container">
 			<div class="tim-row paragraph">
-				<p>First of all, I want to thank Allah for the blessing he has bestowed upon me especially in the form of my good Companions.</p>	
-				<p>It all started with a facebook post which was shared by one of my friend Abdul Rouf Sahib, Where he mentioned the need of a blood donor and that was the first time I donated blood. Alhamdulilah! The second time it was on the recommendation of the same person that I got a chance to donate blood.</p>
-				<p>Recently one of my relatives was in need of blood and I was informed by her family to be ready for they may need blood. I contacted Vijdan Muhammed kaswoosa and requested him to share the appeal for blood donation, which he posted readily on his page J&k now and I got a call from Br. Zulqarnain asking me that he is ready for blood donation. But he had no transport facility as it was too late; I went to pick him up from his home and to my surprise, this was his 7th or 8th blood donation May Allah reward him.</p>	
-				<p>Similarly, there were many occasions where I followed the same procedure. Recently I met a poor family at SHMS hospital who were facing difficulty in finding a Blood Donor. I again asked Vijdan Kawoosa to post the request on his facebook page following which I got many Calls from volunteer blood donors.</p>
-				<p>Meanwhile, an idea struck in my mind why not to store the information of volunteer donors by using google-sheets. As of now, we have started an Initiative “EHSAH-EK ZINDAGI BACHANAY KA”. The motive of this initiative is to provide VOLUNTEER BLOOD DONORS around Jandk and in Delhi as well. A website WWW.EHSAS.IN has been launched for this purpose and an Andriod App will be available soon.</p>
-				<p>I will end up with this quote, Which was posted by one of my facebook friend Waseem Wani “One Individual may die for an idea But that idea will, after his death, Incarnate itself in a thousand lives”.</p>
+				<p>We are you! and we believe in unity and humanity.</p>	
+				<p>Our main focus is to bridge the gap the between volunteer and Needy people around so we are taking the advantage of an digital era. Here we try to serve the people by ours and yours skills and efforts. The main focus is to provide a platform for the people who are enthusiastic to help the people. As of now we have started this initiative to help the patient by providing them precious gift of life [Blood Donation] we are not a part of any political part nor an organisation we are ordinary people like you who try to bring some relief to the suffering people.</p>
+				<p>Apart from this we will promote all other initiatives that are non political and does not violate the scared code of Islam.</p>
+				<p>Help us to help society by your ideas, Suggestion, Skills and Efforts. So give us your valuable suggestion and be part of this initiative at <a href="https://m.me/ehsas.in">Ehsas Suggessions</a></p>	
 			</div>
 		</div>
 	</div>
